7f3746d61a60191cda5b572fd62c36d0f310609d
watzak <kzahirag@gmail.com>
2015-11-09 17:04:51 +0100

    Initial Commit

---
+++ RNInstagramShare/RNInstagramShare.h
@@ -0,0 +1,13 @@
+//
+//  RNInstagramShare.h
+//  RNInstagramShare
+//
+//  Created by watzak on 09/11/15.
+//  Copyright © 2015 watzak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNInstagramShare : NSObject
+
+@end
---
+++ RNInstagramShare/RNInstagramShare.m
@@ -0,0 +1,13 @@
+//
+//  RNInstagramShare.m
+//  RNInstagramShare
+//
+//  Created by watzak on 09/11/15.
+//  Copyright © 2015 watzak. All rights reserved.
+//
+
+#import "RNInstagramShare.h"
+
+@implementation RNInstagramShare
+
+@end
